98a1cb7498eeea6c1705a3ff096a031bbc505b7e
Sandeep <heytssandeep@gmail.com>
2023-09-08 13:46:22 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = "aba6ff9d6de967d5eac6fd79114693cc";
+    const searchInput = document.getElementById("search-input");
+    const searchButton = document.getElementById("search-button");
+    const cityName = document.getElementById("city-name");
+    const weatherInfo = document.getElementById("weather-info");
+  
+    function updateWeather(city) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.cod === 200) {
+            const temperature = Math.round(data.main.temp - 273.15); 
+            cityName.textContent = data.name;
+            weatherInfo.textContent = `${temperature}°C, ${data.weather[0].description}`;
+          } else {
+            cityName.textContent = "City Not Found";
+            weatherInfo.textContent = "";
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
+          cityName.textContent = "City Not Found";
+          weatherInfo.textContent = "";
+        });
+    }
+  
+
+    function handleSearch() {
+      const city = searchInput.value.trim();
+      if (city) {
+        updateWeather(city);
+      }
+    }
+  
+    searchButton.addEventListener("click", handleSearch);
+  
+
+    searchInput.addEventListener("keyup", function (event) {
+      if (event.key === "Enter") {
+        handleSearch();
+      }
+    });
+  
+    
+    updateWeather("dibrugarh");
+  });
+  
+  function updateDateTime() {
+    const dateTimeElement = document.getElementById("datetime");
+    const now = new Date();
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    };
+    const formattedDateTime = now.toLocaleDateString("en-US", options);
+    dateTimeElement.textContent = formattedDateTime;
+  }
+  
+  setInterval(updateDateTime, 1000);
+  
+  updateDateTime();
+  
