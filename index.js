720a039de9a30a9a30b5c209f2bed81f46f8f05c
Sandeep <heytssandeep@gmail.com>
2023-09-08 14:09:06 +0530

    adding the feels like tab

--- index.js
+++ index.js
@@ -1,67 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = "aba6ff9d6de967d5eac6fd79114693cc";
-    const searchInput = document.getElementById("search-input");
-    const searchButton = document.getElementById("search-button");
-    const cityName = document.getElementById("city-name");
-    const weatherInfo = document.getElementById("weather-info");
-  
-    function updateWeather(city) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.cod === 200) {
-            const temperature = Math.round(data.main.temp - 273.15); 
-            cityName.textContent = data.name;
-            weatherInfo.textContent = `${temperature}°C, ${data.weather[0].description}`;
-          } else {
-            cityName.textContent = "City Not Found";
-            weatherInfo.textContent = "";
-          }
-        })
-        .catch((error) => {
-          console.error("Error fetching weather data:", error);
+  const apiKey = "aba6ff9d6de967d5eac6fd79114693cc";
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-button");
+  const cityName = document.getElementById("city-name");
+  const weatherInfo = document.getElementById("weather-info");
+
+  function updateWeather(city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.cod === 200) {
+          const temperature = Math.round(data.main.temp - 273.15);
+          const feelsLike = Math.round(data.main.feels_like - 273.15); // Fetch "feels like" temperature
+          cityName.textContent = data.name;
+          weatherInfo.textContent = `${temperature}°C (Feels like ${feelsLike}°C), ${data.weather[0].description}`;
+        } else {
           cityName.textContent = "City Not Found";
           weatherInfo.textContent = "";
-        });
-    }
-  
-
-    function handleSearch() {
-      const city = searchInput.value.trim();
-      if (city) {
-        updateWeather(city);
-      }
-    }
-  
-    searchButton.addEventListener("click", handleSearch);
-  
-
-    searchInput.addEventListener("keyup", function (event) {
-      if (event.key === "Enter") {
-        handleSearch();
-      }
-    });
-  
-    
-    updateWeather("dibrugarh");
-  });
-  
-  function updateDateTime() {
-    const dateTimeElement = document.getElementById("datetime");
-    const now = new Date();
-    const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-    };
-    const formattedDateTime = now.toLocaleDateString("en-US", options);
-    dateTimeElement.textContent = formattedDateTime;
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        cityName.textContent = "City Not Found";
+        weatherInfo.textContent = "";
+      });
   }
-  
-  setInterval(updateDateTime, 1000);
-  
-  updateDateTime();
-  
+
+  function handleSearch() {
+    const city = searchInput.value.trim();
+    if (city) {
+      updateWeather(city);
+    }
+  }
+
+  searchButton.addEventListener("click", handleSearch);
+
+  searchInput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  });
+
+  updateWeather("dibrugarh");
+});
+
+function updateDateTime() {
+  const dateTimeElement = document.getElementById("datetime");
+  const now = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
+  const formattedDateTime = now.toLocaleDateString("en-US", options);
+  dateTimeElement.textContent = formattedDateTime;
+}
+
+setInterval(updateDateTime, 1000);
+
+updateDateTime();
